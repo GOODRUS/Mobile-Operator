369c8514b116286011f7034ad70fffe25695be85
Dmitriy Shilyaev <goodrus1993@yandex.ru>
2022-10-14 14:20:47 +0500

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int count = 100; // Остаток на счёте
-        int depositAmount = 1100; // Сумма пополнения
+        int depositAmount = 1750; // Сумма пополнения
         int perHundred = 100; // За каждые 100 рублей бонус 1 рубль
 
         int bonus;
-        if(depositAmount > 1000) {
+        if (depositAmount > 1000) {
             bonus = depositAmount / perHundred; // Бонусов за каждые 100 рублей при пополнении свыше 1000 рублей
         } else {
             bonus = 0; // Бонусов при пополнении не более 1000 рублей
         }
 
         System.out.println("На вашем счёте " + (count + depositAmount + bonus) + " рублей.");
+        System.out.println("Бонус " + bonus + " рублей");
     }
 }
